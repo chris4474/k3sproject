b1087d9b67d0870d3b61d7b8eea341174115745d
chris4474 <c237537@yahoo.fr>
2024-08-28 06:23:42 +0000

    Build Alpinme/curl container with Alpine 3.20.2

--- curl/build.sh
+++ curl/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t registry.symphorines.home/chris7444/curl:6.0 --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t registry.symphorines.home/chris7444/curl:7.0 --push .
