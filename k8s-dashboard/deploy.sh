a4e7f29387d3b5e3e12172892e016015a0ab8ed3
chris4474 <c237537@yahoo.fr>
2024-08-29 17:41:06 +0000

    change deploy.sh to accomodate my 3 clusters more easily

--- k8s-dashboard/deploy.sh
+++ k8s-dashboard/deploy.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+cluster=$(kubectl config current-context)
+read -t 5 -p "About to deploy k8s dashboard on cluster $cluster. Are you OK ? Y,N [N] " answer
+if [ "$answer" != "Y" ] && [ "$answer" != "y" ]
+then
+  echo Bye
+  exit
+fi
+
 kubectl apply -f namespace.yaml
 
 kubectl create secret generic --namespace k8s-dashboard apps-certificate \
     --from-file=tls.key=$HOME/certs/apps/apps.key \
     --from-file=tls.crt=<(cat $HOME/certs/apps/apps.crt $HOME/certs/apps/intCA.crt)
 
+cluster=$cluster envsubst <values.yaml.tpl >values.yaml
 helm upgrade --install k8s-dashboard kubernetes-dashboard/kubernetes-dashboard \
    --namespace k8s-dashboard \
    -f values.yaml
---
+++ k8s-dashboard/values.yaml.tpl
@@ -0,0 +1,17 @@
+app:
+  ingress:
+    enabled: true
+    hosts:
+      - k8s-dashboard.${cluster}.symphorines.home
+    ingressClassName: traefik
+    useDefaultAnnotations: false
+    tls:
+      enabled: true
+      secretName: apps-certificate
+
+kong:
+  proxy:
+    annotations:
+      traefik.ingress.kubernetes.io/service.serversscheme: https
+      traefik.ingress.kubernetes.io/service.serverstransport: k8s-dashboard-skipverify@kubernetescrd
+
