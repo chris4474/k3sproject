1ae2d95f6659c47e8a2346bd4031b76b76d584c8
chris4474 <c237537@yahoo.fr>
2024-09-01 10:22:43 +0000

    Cleaned up unused files in the ansible directory

--- ansible/deploy/scripts/log2ram.sh
+++
@@ -1,4 +0,0 @@
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
-sudo apt update
-sudo apt install log2ram
--- ansible/deploy/scripts/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eu
-delay=$(( $RANDOM % 20 ))
-echo Back in $delay seconds
-sleep $delay
-rc=$(( $RANDOM % 2 ))
-exit $rc
