ab577b9ed6217f020f081247c1d3d09f4aec3d2d
chris4474 <c237537@yahoo.fr>
2024-09-01 10:23:33 +0000

    does not redirect http traefik to the traefik console as it does not work for now

--- traefik/dashboard/deploy.sh
+++ traefik/dashboard/deploy.sh
@@ -13,13 +13,13 @@ kubectl create secret generic -n kube-system apps-tls-secret \
     --from-file=tls.crt=<(cat $HOME/certs/apps/apps.crt $HOME/certs/apps/intCA.crt)
 
 
-cluster=$cluster envsubst <ingressroute-redirect.yaml.tpl >ingressroute-redirect.yaml
-kubectl apply -f ingressroute-redirect.yaml
+#cluster=$cluster envsubst <ingressroute-redirect.yaml.tpl >ingressroute-redirect.yaml
+#kubectl apply -f ingressroute-redirect.yaml
 
 kubectl apply -f service.yaml
 kubectl apply -f users.yaml
 kubectl apply -f middleware-auth.yaml
-kubectl apply -f middleware-redirect.yaml
+#kubectl apply -f middleware-redirect.yaml
 kubectl apply -f middleware-rewrite.yaml 
 cluster=$cluster envsubst < ingressroute-tls.yaml.tpl >ingressroute-tls.yaml
 kubectl apply -f ingressroute-tls.yaml
