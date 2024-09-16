9df1553e99a6ad2ab3a03e6f70ef21dc24a4bd84
chris4474 <c237537@yahoo.fr>
2024-09-16 17:29:36 +0000

    Add a few troubleshooting tools in container

--- applis/utilities/build.sh
+++ applis/utilities/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.symphorines.home/chris7444/utilities:1.0 --push .
+docker  buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.symphorines.home/chris7444/utilities:v2.0 --push .
