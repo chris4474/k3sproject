d819df691e092031cffbc06e66c4520de13ef599
chris4474 <me.example.com>
2025-03-04 19:36:34 +0100

    update docker image 'utilities'

--- applis/utilities/build.sh
+++ applis/utilities/build.sh
@@ -1,1 +1,1 @@
-docker  buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.symphorines.home/chris7444/utilities:v3.0 --push .
+docker  buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.symphorines.home/chris7444/utilities:v4.0 --push .
