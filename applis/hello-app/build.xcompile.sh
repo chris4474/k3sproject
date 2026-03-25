a90f85eaf7aa83780b2b1341f97e870a4bc8c387
chris4474 <me.example.com>
2026-03-25 09:54:22 +0100

    hello-app: apply same tag to images

--- applis/hello-app/build.xcompile.sh
+++ applis/hello-app/build.xcompile.sh
@@ -1,2 +1,2 @@
 registry=${DEFAULT_REGISTRY:-registry.symphorines.home}
-docker buildx build -f Dockerfile.xcompile --platform linux/amd64,linux/arm64 -t ${registry}/chris7444/hello-app:v9.3 --push .
+docker buildx build -f Dockerfile.xcompile --platform linux/amd64,linux/arm64 -t ${registry}/chris7444/hello-app:short-v9.2 --push .
