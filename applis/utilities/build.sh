21d1190aebd63c55809eb4ab61a9f4cd09128261
chris4474 <me.example.com>
2026-03-28 15:48:47 +0100

    tools/utilities: allow image push to new registry (zot)

--- applis/utilities/build.sh
+++ applis/utilities/build.sh
@@ -1,1 +1,2 @@
-docker  buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t registry.symphorines.home/chris7444/utilities:v4.0 --push .
+registry=${DEFAULT_REGISTRY:-registry.symphorines.home}
+docker  buildx build --platform linux/amd64,linux/arm64 -t ${registry}/chris7444/utilities:v4.0 --push .
