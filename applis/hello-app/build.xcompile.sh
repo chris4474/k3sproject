57be56d7d9fc67b353731d927a634ff01f9ce530
chris4474 <me.example.com>
2026-04-01 02:30:33 +0200

    reflect project layout in my gitea repo

--- applis/hello-app/build.xcompile.sh
+++ applis/hello-app/build.xcompile.sh
@@ -1,2 +1,2 @@
 registry=${DEFAULT_REGISTRY:-registry.symphorines.home}
-docker buildx build -f Dockerfile.xcompile --platform linux/amd64,linux/arm64 -t ${registry}/chris7444/hello-app:short-v9.2 --push .
+docker buildx build -f Dockerfile.xcompile --platform linux/amd64,linux/arm64 -t ${registry}/chris/hello-app:short-v9.2 --push .
