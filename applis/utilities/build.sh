c0f87fcfe004758c0f0f2525cf43f640fb03d08d
chris4474 <me.example.com>
2026-04-04 20:47:40 +0200

    utilities container image stored in my gitea repository

--- applis/utilities/build.sh
+++ applis/utilities/build.sh
@@ -1,2 +1,2 @@
 registry=${DEFAULT_REGISTRY:-registry.symphorines.home}
-docker  buildx build --platform linux/amd64,linux/arm64 -t ${registry}/chris7444/utilities:v4.0 --push .
+docker  buildx build --platform linux/amd64,linux/arm64 -t ${registry}/chris/utilities:v4.0 --push .
